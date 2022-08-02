f1270f11e5c9b6e7f2aa1fc36735c461571c3b08
DaveP80 <david654@revature.net>
2022-08-01 21:25:50 -0400

    simple

---
+++ src/App.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class App {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Please enter the price in dollars and cents $$.cc");
+
+        while (!input.hasNextDouble()) {
+            input.nextLine();
+        }
+        double price = input.nextDouble();
+
+        System.out.println("Please enter another price in dollars and cents $$.cc");
+
+        while (!input.hasNextDouble()) {
+            input.nextLine();
+        }
+            double price2 = input.nextDouble();
+
+        double nums[] = {price, price2};
+        double result = 0;
+        int count = nums.length;
+        for (double i: nums) result += i;
+
+        System.out.println("the average price is " + result/(nums.length));
+        System.out.println("the total price is " + result);
+
+    }
+}

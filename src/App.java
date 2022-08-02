7bbc91b5614a4aefafaf4a7e71c664bdef69cecb
DaveP80 <david654@revature.net>
2022-08-01 23:13:25 -0400

    simple

--- src/App.java
+++ src/App.java
@@ -5,27 +5,31 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Please enter the price in dollars and cents $$.cc");
+            while (true) {
 
-        while (!input.hasNextDouble()) {
-            input.nextLine();
-        }
-        double price = input.nextDouble();
+            System.out.println("Please enter the price in dollars and cents $$.cc");
 
-        System.out.println("Please enter another price in dollars and cents $$.cc");
+            while (!input.hasNextDouble()) {
+                input.nextLine();
+            }
+            double price = input.nextDouble();
 
-        while (!input.hasNextDouble()) {
-            input.nextLine();
-        }
+            System.out.println("Please enter another price in dollars and cents $$.cc");
+
+            while (!input.hasNextDouble()) {
+                input.nextLine();
+            }
             double price2 = input.nextDouble();
 
-        double nums[] = {price, price2};
-        double result = 0;
-        int count = nums.length;
-        for (double i: nums) result += i;
+            double nums[] = {price, price2};
+            double result = 0;
+            int count = nums.length;
+            for (double i : nums) result += i;
 
-        System.out.println("the average price is " + result/(nums.length));
-        System.out.println("the total price is " + result);
+            System.out.println("the average price is " + result / nums.length);
+            System.out.println("the total price is " + result);
+            System.out.println();
 
+        }
     }
 }
